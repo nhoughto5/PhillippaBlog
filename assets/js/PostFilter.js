61da5565998f9e6480b0e3f0412b0eea8230f43f
Nick Houghton <nhoughto5@gmail.com>
2017-09-17 11:45:45 -0700

    Have post filter working

---
+++ assets/js/PostFilter.js
@@ -0,0 +1,17 @@
+---
+---
+function filter(category) {
+  console.log("Called: " + category);
+  let list = document.getElementById("postList");
+  let posts = list.children;
+  for(let i = 0; i < posts.length; ++i){
+    console.log(posts[i]);
+    if(category === "All"){
+      posts[i].style.display = "block";
+    }
+    else if(posts[i].dataset.category !== category){
+      posts[i].style.display = "none";
+
+    }
+  }
+};
