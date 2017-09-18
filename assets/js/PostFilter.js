888a53a03f84cfab67b43b1120078c82040b06fa
Nick Houghton <nhoughto5@gmail.com>
2017-09-17 17:36:04 -0700

    Added editable

--- assets/js/PostFilter.js
+++ assets/js/PostFilter.js
@@ -1,11 +1,9 @@
 ---
 ---
 function filter(category) {
-  console.log("Called: " + category);
   let list = document.getElementById("postList");
   let posts = list.children;
   for(let i = 0; i < posts.length; ++i){
-    console.log(posts[i]);
     if(category === "All" || posts[i].dataset.category === category){
       posts[i].style.display = "block";
     }
